be49bbd35df5474dd023c5e2b542170ee9aec89b
Thiago Mattos <euthiagomattos@gmail.com>
2024-10-16 17:54:38 -0300

    added tables to db

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const connection = require("./database/database");
 const categoriesController = require("./categories/CategoriesController");
 const articlesController = require("./articles/ArticlesController");
 
+const Article = require("./articles/Articles");
+const Category = require("./categories/Category");
 
 //view engine
 app.set('view engine', 'ejs');
