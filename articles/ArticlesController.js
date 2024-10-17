0b8ad07ae80424c5a14b36cead63364857ce915f
Thiago Mattos <euthiagomattos@gmail.com>
2024-10-17 14:21:00 -0300

    added list and delete article

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -5,13 +5,17 @@ const Article = require("./Articles");
 const slugify = require("slugify");
 
 router.get("/admin/articles", (req,res) =>{
-    res.send("ARTICLES ROUTES")
+    Article.findAll({
+        include: [{model: Category}]
+    }).then(articles =>{
+        res.render("admin/articles/index",{articles: articles});
+    });
 });
 
 router.get("/admin/articles/new", (req,res) =>{
     Category.findAll().then(categories =>{
         res.render("admin/articles/new", {categories: categories});
-    })   
+    });   
 });
 
 router.post("/articles/save", (req,res) =>{
@@ -25,8 +29,28 @@ router.post("/articles/save", (req,res) =>{
         body: body,
         categoryId: category
     }).then(()=>{
-        res.redirect("/admin/articles")
-    })
+        res.redirect("/admin/articles");
+    });
+});
+
+router.post("/articles/delete", (req,res)=>{
+    var id = req.body.id
+    if(id != undefined){
+        if(!isNaN(id)){ 
+            Article.destroy({
+                where:{
+                    id: id
+                }
+            }).then(()=>{
+                res.redirect("/admin/articles");
+            });
+
+        }else{ // Not a Number
+            res.redirect("/admin/articles");
+        }
+    }else{ // NULL
+        res.redirect("/admin/articles");
+    }
 });
 
 module.exports = router;
