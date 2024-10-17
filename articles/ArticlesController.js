2ca1810917b277f1a7dd144cb4993e0b27b3b929
Thiago Mattos <euthiagomattos@gmail.com>
2024-10-17 16:56:20 -0300

    added edit article

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -53,4 +53,32 @@ router.post("/articles/delete", (req,res)=>{
     }
 });
 
+router.get("/admin/articles/edit/:id", (req,res)=>{
+    var id = req.params.id;
+    Article.findByPk(id).then(article =>{
+        if(article != undefined){
+
+            Category.findAll().then(categories =>{
+                res.render("admin/articles/edit", {categories: categories, article, article})   ;
+            });
+        }else{
+            res.redirect("/");
+        }
+    }).catch(error =>{
+        res.redirect("/");
+    });
+});
+
+router.post("/articles/update", (req,res)=>{
+    var id = req.body.id;
+    var title = req.body.title;
+
+    Article.update({title: title, slug: slugify(title)}, {
+        where:{
+            id: id
+        }
+    }).then(() =>{
+        res.redirect("/admin/articles");
+    });
+});
 module.exports = router;
--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const articlesController = require("./articles/ArticlesController");
 
 const Article = require("./articles/Articles");
 const Category = require("./categories/Category");
+const { where } = require("sequelize");
 
 //view engine
 app.set('view engine', 'ejs');
@@ -38,7 +39,9 @@ app.get("/", (req,res) =>{
             ['id', 'DESC']
         ]
     }).then(articles =>{
-        res.render("index",{articles: articles});
+        Category.findAll().then(categories =>{
+            res.render("index",{articles: articles, categories: categories});
+        });
     });
 });
 
@@ -50,14 +53,38 @@ app.get("/:slug", (req,res) =>{
         }
     }).then(article =>{
         if(article != undefined){
-            res.render("article",{article: article});
+            Category.findAll().then(categories =>{
+                res.render("article",{article: article, categories: categories});
+            });
         }else{
             res.redirect("/");
         }
     }).catch(error =>{
         res.redirect("/");
-    })
-})
+    });
+});
+
+app.get("/category/:slug", (req,res) =>{
+    var slug = req.params.slug;
+    Category.findOne({
+        where:{
+            slug: slug
+        },
+        include: [{model: Article}]
+    }).then(category =>{
+        if(category != undefined){
+
+            Category.findAll().then(categories =>{
+                res.render("index",{articles: category.articles, categories: categories})
+            })
+
+        }else{
+            res.redirect("/");
+        }
+    }).catch(error =>{
+        res.redirect("/");
+    });
+});
 
 app.listen(8080, (req,res) =>{
     console.log("running");
