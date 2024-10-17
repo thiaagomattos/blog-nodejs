9fc6fad92c30113fe602ee26c845583adb3dca43
Thiago Mattos <euthiagomattos@gmail.com>
2024-10-17 14:01:46 -0300

    created articles create

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const Category = require("../categories/Category");
+const Article = require("./Articles");
+const slugify = require("slugify");
 
-router.get("/articles", (req,res) =>{
+router.get("/admin/articles", (req,res) =>{
     res.send("ARTICLES ROUTES")
 });
 
@@ -10,6 +12,21 @@ router.get("/admin/articles/new", (req,res) =>{
     Category.findAll().then(categories =>{
         res.render("admin/articles/new", {categories: categories});
     })   
+});
+
+router.post("/articles/save", (req,res) =>{
+    var title = req.body.title;
+    var body = req.body.body;
+    var category = req.body.category;
+
+    Article.create({
+        title: title,
+        slug: slugify(title),
+        body: body,
+        categoryId: category
+    }).then(()=>{
+        res.redirect("/admin/articles")
+    })
 });
 
 module.exports = router;
--- database/database.js
+++ database/database.js
@@ -2,7 +2,8 @@ const Sequelize = require("sequelize");
 
 const connection = new Sequelize('db_blog','root','123456',{
     host:'localhost',
-    dialect: 'mysql'
+    dialect: 'mysql',
+    timezone: "-03:00"
 });
 
 module.exports = connection;
