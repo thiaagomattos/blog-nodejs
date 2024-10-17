680e5dbc85174e6e150486ef4d4ea5754720ca0d
Thiago Mattos <euthiagomattos@gmail.com>
2024-10-17 11:07:04 -0300

    created delete category

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -16,20 +16,41 @@ router.post("/categories/save", (req,res) =>{
             slug: slugify(title)
         }).then(() =>{
             res.redirect("/");
-        })
+        });
         
 
     }else{
         res.redirect("admin/categories/new");
     }
-})
+});
 
 router.get("/admin/categories", (req,res) =>{
 
     Category.findAll().then(categories =>{
         res.render("admin/categories/index", {categories: categories});
-    })
+    });
 
-})
+});
+
+router.post("/categories/delete", (req,res)=>{
+    var id = req.body.id
+    if(id != undefined){
+        if(!isNaN(id)){ 
+
+            Category.destroy({
+                where:{
+                    id: id
+                }
+            }).then(()=>{
+                res.redirect("/admin/categories");
+            });
+
+        }else{ // Not a Number
+            res.redirect("/admin/categories");
+        }
+    }else{ // NULL
+        res.redirect("/admin/categories");
+    }
+});
 
 module.exports = router;
